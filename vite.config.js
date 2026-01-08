28f89497e0ccc4bd5cde60a538e3a99266139bbe
TonyMPCastro <antonio.m.fg38@outlook.com>
2026-01-08 16:58:01 -0300

    pages

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/Realidade_Aumentada_React/', // O nome exato do seu repositório
+})
